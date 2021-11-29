753d2f5493ba69430437536d8a22584b936a8015
Justin <justin.t.carroll1@gmail.com>
2021-11-29 16:20:36 +0100

    29/11/2021 Pre-production Version 0.8.0
    -Updated application properties.
    -Added the controller, filter and entry point for token authentication.
    -Defined the utilities for Jwt.
    -Introduced the signup and login requests.
    -Added config for web security.
    -Test controller added.
    -Responses for messages and Jwt.
    
    Troubleshooting successful, but db not connecting yet.

--- src/main/java/carroll/tbel/tripupapplicationbackend/controllers/TestController.java
+++ src/main/java/carroll/tbel/tripupapplicationbackend/controllers/TestController.java
@@ -1,4 +1,36 @@
 package carroll.tbel.tripupapplicationbackend.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
 public class TestController {
+
+    @GetMapping
+    public String allAccess() {
+        return "Public Content.";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('AGENT') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User Content.";
+    }
+
+    @GetMapping("/agent")
+    @PreAuthorize("hasRole('AGENT')")
+    public String agentAccess() {
+        return "Agent Board.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin Board.";
+    }
 }
